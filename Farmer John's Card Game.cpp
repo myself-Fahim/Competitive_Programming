17f1a1769c25d911d9198c6e13ce2523672549da
myself-Fahim <ashfahim18@gmail.com>
2025-02-22 22:23:53 +0600

    New Problem solved

---
+++ Farmer John's Card Game.cpp
@@ -0,0 +1,98 @@
+/* In the name of Allah, Most Gracious, Most Merciful*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define cint cin >> t
+#define whl while (t--)
+#define vi vector<int>
+#define vll vector<ll>
+#define Y "YES"
+#define N "NO"
+#define ascii(n) static_cast<int>(n);
+const int MOD = 1e9 + 7; // Example modulus for large numbers
+const int INF = INT_MAX; // Infinity representation
+#define for0(n) for (int i = 0; i < (n); i++)
+#define for1(n) for (int i = 1; i < (n); i++)
+#define fore1(n) for (int i = 1; i <= (n); i++)
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+bool cmp(const pair<int, int> &p1, const pair<int, int> &p2)
+{
+    if (p1.first < p2.first)
+        return 1;
+    else if (p1.first == p2.first)
+        return (p1.second > p2.second);
+    else
+        return 0;
+    // Or *(-1) with p.first
+}
+// for (auto &u : s)
+// {
+//     u = tolower(static_cast<unsigned char>(u));
+// }
+
+void Ahlan_Wa_Sahlan()
+{
+    optimize();
+    int t;
+    cin >> t;
+    whl
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> v(n, vector<int>(m));
+        vector<vector<int>> v2(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> v[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            sort(v[i].begin(), v[i].end());
+        }
+        v2 = v;
+        vi v3;
+        sort(v.begin(), v.end());
+        for (int j = 0; j < m; j++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                v3.push_back(v[i][j]);
+            }
+        }
+        bool flag = false;
+        for (int i = 0; i < v3.size() - 1; i++)
+        {
+            if (v3[i] > v3[i + 1])
+            {
+                flag = true;
+            }
+        }
+        if (flag)
+            cout << "-1" << endl;
+        else
+        {
+            vector<pair<int, int>> p(n);
+            for (int i = 0, j = 0; i < n; i++)
+            {
+                p[i] = {v2[i][j], i};
+            }
+            sort(p.begin(), p.end());
+            for (auto u : p)
+            {
+                cout << u.second + 1 << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+int main()
+{
+    Ahlan_Wa_Sahlan();
+}
