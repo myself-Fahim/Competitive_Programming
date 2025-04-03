af36c57c371091a585bd09ad0708312cad261567
myself-Fahim <ashfahim18@gmail.com>
2025-04-04 00:01:52 +0600

    New Solved

---
+++ Li hua and pattern.cpp
@@ -0,0 +1,78 @@
+/* In the name of Allah, Most Gracious, Most Merciful*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define cint cin >> t
+#define whl while (t--)
+#define vi vector<int>
+#define vll vector<ll>
+#define Y "YES"
+#define N "NO"
+#define ascii(n) static_cast<int>(n);
+const int MOD = 1e9 + 7; // Example modulus for large numbers
+const int INF = INT_MAX; // Infinity representation
+#define for0(n) for (int i = 0; i < (n); i++)
+#define for1(n) for (int i = 1; i < (n); i++)
+#define fore1(n) for (int i = 1; i <= (n); i++)
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+bool cmp(const pair<int, int> &p1, const pair<int, int> &p2)
+{
+    if (p1.first < p2.first)
+        return 1;
+    else if (p1.first == p2.first)
+        return (p1.second > p2.second);
+    else
+        return 0;
+    // Or *(-1) with p.first
+}
+// for (auto &u : s)
+// {
+//     u = tolower(static_cast<unsigned char>(u));
+// }
+
+void Ahlan_Wa_Sahlan()
+{
+
+    int t;
+    cin >> t;
+    whl
+    {
+        ll n, k, cnt = 0;
+        cin >> n >> k;
+        ll arr[n + 10][n + 10];
+        ll arr2[n + 10][n + 10];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[i][j] != arr[n - 1 - i][n - 1 - j])
+                {
+                    cnt++;
+                }
+            }
+        }
+        cnt /= 2;
+        if (cnt > k)
+            cout << N << endl;
+        else if (((k - cnt) & 1) && n%2==0)
+            cout << N << endl;
+        else
+            cout << Y << endl;
+    }
+}
+
+int main()
+{
+    Ahlan_Wa_Sahlan();
+}
