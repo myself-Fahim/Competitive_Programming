9e34fff7edab8f20b319fd05834946f50d85283d
myself-Fahim <ashfahim18@gmail.com>
2025-05-15 22:04:55 +0600

    New Solved

---
+++ Min = Gcd.cpp
@@ -0,0 +1,91 @@
+/* In the name of Allah, Most Gracious, Most Merciful*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define cint cin >> t
+#define whl while (t--)
+#define vi vector<int>
+#define vll vector<ll>
+#define Y "YES"
+#define N "NO"
+#define ascii(n) static_cast<int>(n);
+const int MOD = 1e9 + 7; // Example modulus for large numbers
+const int INF = INT_MAX; // Infinity representation
+#define for0(n) for (ll int i = 0; i < (n); i++)
+#define for1(n) for (int i = 1; i < (n); i++)
+#define fore1(n) for (int i = 1; i <= (n); i++)
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+bool cmp(const pair<int, int> &p1, const pair<int, int> &p2)
+{
+    if (p1.first < p2.first)
+        return 1;
+    else if (p1.first == p2.first)
+        return (p1.second > p2.second);
+    else
+        return 0;
+    // Or *(-1) with p.first
+}
+// for (auto &u : s)
+// {
+//     u = tolower(static_cast<unsigned char>(u));
+// }
+void Ahlan_Wa_Sahlan()
+{
+    optimize();
+    int t;
+    cin >> t;
+    whl
+    {
+        int n;
+        cin >> n;
+        vll a(n);
+        vll x;
+        vll y;
+        for0(n)
+        {
+            cin >> a[i];
+        }
+        ll minn = min_element(a.begin(), a.end()) - a.begin();
+        ll min_value = a[minn];
+        for0(n)
+        {
+            if (i != minn && a[i] % min_value == 0)
+            {
+                y.push_back(a[i]);
+            }
+            else
+                x.push_back(a[i]);
+        }
+        // for (auto u : y)
+        //     cout << u << " ";
+        // cout << endl;
+
+        // for (auto u : x)
+        //     cout << u << " ";
+        // cout << endl;
+
+        if (y.empty())
+            cout << N << endl;
+        else
+        {
+            ll gcdd = y[0];
+            for (int i = 1; i < y.size(); i++)
+            {
+                gcdd = gcd(gcdd, y[i]);
+            }
+            // cout<<gcdd<<" "<<min_value<<endl;
+            if (gcdd == min_value)
+                cout << Y << endl;
+            else
+                cout << N << endl;
+        }
+    }
+}
+int main()
+{
+    Ahlan_Wa_Sahlan();
+}
