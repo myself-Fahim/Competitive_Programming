13e78a59f7912f587f74d42841e81dda13a9cd15
myself-Fahim <ashfahim18@gmail.com>
2025-05-21 23:08:04 +0600

    New Solved

---
+++ Skibidus and Fanum Tax.cpp
@@ -0,0 +1,75 @@
+/* In the name of Allah, Most Gracious, Most Merciful*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define cint cin >> t
+#define whl while (t--)
+#define vi vector<int>
+#define vll vector<ll>
+#define Y "YES"
+#define N "NO"
+#define ascii(n) static_cast<int>(n);
+const int MOD = 1e9 + 7; // Example modulus for large numbers
+const int INF = INT_MAX; // Infinity representation
+#define for0(n) for (ll int i = 0; i < (n); i++)
+#define for1(n) for (int i = 1; i < (n); i++)
+#define fore1(n) for (int i = 1; i <= (n); i++)
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+bool cmp(const pair<int, int> &p1, const pair<int, int> &p2)
+{
+    if (p1.first < p2.first)
+        return 1;
+    else if (p1.first == p2.first)
+        return (p1.second > p2.second);
+    else
+        return 0;
+    // Or *(-1) with p.first
+}
+// for (auto &u : s)
+// {
+//     u = tolower(static_cast<unsigned char>(u));
+// }
+void Ahlan_Wa_Sahlan()
+{
+    optimize();
+    int t;
+    cin >> t;
+    whl
+    {
+        ll n, m, x;
+        cin >> n >> m;
+        vll a(n);
+        for0(n)
+        {
+            cin >> a[i];
+        }
+        cin >> x;
+        ll f = 0;
+        ll maxx_ele = min(a[0], (x - a[0]));
+        for (int i = 1; i < n; i++)
+        {
+            if ((min(a[i], x - a[i])) >= maxx_ele)
+                maxx_ele = (min(a[i], x - a[i]));
+            else if ((max(a[i], x - a[i])) >= maxx_ele)
+                maxx_ele = (max(a[i], x - a[i]));
+            else
+            {
+                f = 1;
+                break;
+            }
+        }
+
+        if (f)
+            cout << N << endl;
+        else
+            cout << Y << endl;
+    }
+}
+int main()
+{
+    Ahlan_Wa_Sahlan();
+}
